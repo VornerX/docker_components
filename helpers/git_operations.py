5ed00abcbd35e3ffc7ee9b849b0903b57c35cef4
Yurii Smolkin <yuriy.smolkin@sandsiv.com>
2017-11-08 12:59:36 +0200

    __pycache__ added to .gitignore

--- helpers/git_operations.py
+++ helpers/git_operations.py
@@ -1,19 +1,16 @@
 import os
-import shutil
 from git import Repo
 from helpers.color_print import ColorPrint
-from config import GIT_REPOSITORIES, HOME_DEPLOYMENT_DIR
+from config import GIT_REPOSITORIES
 
 cprint = ColorPrint()
 
 
 def clone_repositories():
-    if os.path.exists(HOME_DEPLOYMENT_DIR):
-        shutil.rmtree(HOME_DEPLOYMENT_DIR)
 
     for component_name, repo_settings in GIT_REPOSITORIES.items():
 
-        os.makedirs(repo_settings['local_dir'], exist_ok=True)
+        os.makedirs(repo_settings['local_dir'], exist_ok=True, mode=0o777)
 
         # TODO: if dir with repo already exists - do something
         Repo.clone_from(
